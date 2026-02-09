8239b1f84dce9eb3f2677c58a08917b587c0cb0b
LeBlob2 <leblob2@proton.me>
2026-02-09 11:02:32 +0800

    Path to passwords directory now made

--- create_database.py
+++ create_database.py
@@ -2,17 +2,19 @@ import sqlite3
 import os
 from pathlib import Path
 
-#Finds the Documents directory and puts it in there
-path = os.path.abspath(os.path.dirname(__file__))
+#Finds the Documents and Passwords directory
+path = os.path.expanduser("~/Documents/Passwords")
 database = os.path.join(path, "Passwords.db")
 
 #for debugging delete after build
 print("Documents directory is:", path)
+
+#makes sure we don't make the same folder again
 if not os.path.exists(path):
     os.makedirs(path)
 
 
-with sqlite3.connect('Passwords.db') as connection: #This stores it in the python directory for now but should be in Documents in later versions
+with sqlite3.connect(database) as connection: #This stores it in the python directory for now but should be in Documents in later versions
 
       cursor = connection.cursor()
       cursor.execute("DROP TABLE IF EXISTS Passwords")
@@ -29,4 +31,6 @@ with sqlite3.connect('Passwords.db') as connection: #This stores it in the pytho
       """
       cursor.execute(create_table_query)
       connection.commit()
+
       print("Database and Table made") #For debug only use a message window for the real thing
+      
