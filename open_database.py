7a261b9f757ab1b58fe3421681a43cdb61a6d936
LeBlob2 <leblob2@proton.me>
2026-02-09 13:52:02 +0800

    removed

--- open_database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-# This file is for opening and writing to an exsiting database
-
-path = os.path.expanduser("~/Documents/Passwords")
-database = os.path.join(path, "Passwords.db")
-
-with sqlite3.connect(database) as connection:
-    cursor = connection.cursor()
